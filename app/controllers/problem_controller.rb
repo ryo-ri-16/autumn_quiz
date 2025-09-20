f9d4181249f8f32b10835cc19bcc4ebcb37799e9
ryo-ri-16 <ujiro1998@gmail.com>
2025-09-20 16:14:19 +0900

    初期画面

--- app/controllers/problem_controller.rb
+++ app/controllers/problem_controller.rb
@@ -2,4 +2,13 @@ class ProblemController < ApplicationController
   def index
     @problems = Problem.all
   end
+
+  def show
+  end
+
+  def answer
+  end
+
+  def result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "problem#index"
   resources :problems, only: [ :index, :show ] do
+    collection do
+      get :random
+    end
     member do
       post :answer
     end
